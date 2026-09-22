94e49e0970913190dbadddc90066531c5e81c6dc
Dmcdominic <dmcdominic@gmail.com>
2026-09-21 22:14:47 -0700

    Failed attempt to test a grid for 2+ total possible solutions

--- scripts/sudoku.js
+++ scripts/sudoku.js
@@ -25,9 +25,36 @@ logGrid(sudokuAnswerKey);
 populateRecursively(sudokuAnswerKey, 0, 0);
 console.log(sudokuAnswerKey);
 logGrid(sudokuAnswerKey);
+$("#testGrid").html(getGridStr(sudokuAnswerKey, "<br>"));
 var endingTime = Date.now();
 console.log("Numbers tested: " + numbersTested);
 console.log("Processing time: " + (endingTime - startingTime) + "ms");
+
+
+// TODO - CLEANUP - OLD (see notes over populateRecursivelyWithNumSolutions())
+// console.log("Testing populateRecursivelyWithNumSolutions()")
+// var sudokuAnswerKey2 = [];
+// for (var y = 0; y < 9; y++)
+// {
+//     sudokuAnswerKey2.push([]);
+//     for (var x = 0; x < 9; x++)
+//     {
+//         sudokuAnswerKey2[y].push(null);
+//     }
+// }
+// numbersTested = 0;
+// startingTime = Date.now();
+// var solved, numSolutions;
+// [solved, numSolutions] = populateRecursivelyWithNumSolutions(sudokuAnswerKey2, 0, 0);
+// console.log("solved: " + solved);
+// console.log("numSolutions: " + numSolutions);
+// console.log(sudokuAnswerKey2);
+// logGrid(sudokuAnswerKey2);
+// endingTime = Date.now();
+// console.log("Numbers tested: " + numbersTested);
+// console.log("Processing time: " + (endingTime - startingTime) + "ms");
+
+
 
 // ================= FINDING MINIMAL STARTING GRID WITH SINGULAR SOLUTION =================
 // TODO
@@ -75,10 +102,61 @@ function populateRecursively(grid, x, y)
     return false;
 }
 
+// TODO - CLEANUP - OLD: This is a horribly inefficient way to do this because it proceeds linearly and is basically brute-force,
+//   so it's gonna take a lot of backtracking to ever reach a grid that COULD have a (different) solution.
+//   My conclusion is that this exercise (at least with this approach) was never really viable, and a smart solver is the right path from the start.
+function populateRecursivelyWithNumSolutions(grid, x, y, priorSolutions = 0)
+{
+    var numbersShuffled = get1To9Shuffled();
+    for (var a = 0; a < numbersShuffled.length; a++)
+    {
+        var num = numbersShuffled[a];
+        numbersTested++;
+        if (isNumValidForCell(grid, num, x, y))
+        {
+            grid[y][x] = num;
+            if (x == 8 && y == 8)
+            {
+                return [true, priorSolutions + 1];
+            }
+            var y2 = y;
+            var x2 = x + 1;
+            if (x2 == 9)
+            {
+                x2 = 0;
+                y2 = y + 1;
+            }
+
+            var solved, numSolutions;
+            [solved, numSolutions] = populateRecursivelyWithNumSolutions(grid, x2, y2, priorSolutions);
+            if (solved && numSolutions >= 2)
+            // if (solved && numSolutions >= 1) // Temporarily ending after only 1 solution is found
+            {
+                return [true, numSolutions];
+            }
+            else if (solved && numSolutions == 1)
+            {
+                priorSolutions++;
+            }
+            else if (solved)
+            {
+                console.error("populateRecursivelyWithNumSolutions() was solved but counts 0 solutions. Logic must be wrong...");
+            }
+            grid[y][x] = null;
+        }
+    }
+    return [false, priorSolutions];
+}
+
 function logGrid(grid)
 {
+    console.log(getGridStr(grid));
+}
+
+function getGridStr(grid, newline = "\n")
+{
     var gridStr = "";
-    const ASCII_HORIZONTAL_BAR = "|-----------------|\n";
+    const ASCII_HORIZONTAL_BAR = "|-----------------|" + newline;
     for (var y = 0; y < 9; y++)
     {
         if (y % 3 == 0)
@@ -91,10 +169,10 @@ function logGrid(grid)
             var num = grid[y][x];
             gridStr += (num == null) ? "?" : num;
         }
-        gridStr += "|\n";
+        gridStr += "|" + newline;
     }
     gridStr += ASCII_HORIZONTAL_BAR;
-    console.log(gridStr);
+    return gridStr;
 }
 
 function get1To9Shuffled()
